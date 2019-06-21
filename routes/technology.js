beeaa1534829cb74b21c4aa0bcd5cb0dfa54fcb8
daisamgi <daira.samboni@carvajal.com>
2019-06-21 16:33:24 -0500

    Noticias desde MySql y redireccionar con Id de noticia en navegador

--- routes/technology.js
+++ routes/technology.js
@@ -1,11 +1,23 @@
-//const connection = require("../mysql.conf").connection;
+const connection = require("../mysql.conf").connection;
 
 const express = require('express');
 const app = express();
+let newsTech = [{}]
 
 app.get('/technology', function (req, res) {
     res.render('technology/index', {
         title: "EL Profeta | Technology"
+    });
+});
+app.get('/Technology/:id', (req, res) => {
+    connection.query(`SELECT * FROM news n inner join newstype nt on n.id_newstype=nt.id WHERE n.id = ${req.params.id}`, (error, results, fields) => {
+    if (error) { throw error };
+    newsTech=results;
+    //console.log(newsTech)
+    });
+    res.render('technology/index', {
+        title: "EL Profeta | Technology",
+        newsTech
     });
 });
 
